661135d5d8ca725953944570453397c34c578a79
m.heezen <m.heezen@secondfloor.com>
2019-11-28 18:25:53 +0100

    rewrote certs command script to use openssl

--- templates/puppet_certs_command.ps1
+++ templates/puppet_certs_command.ps1
@@ -2,44 +2,50 @@ $cert_path = '<%= @cert_path %>'
 $key_path = '<%= @key_path %>'
 $cert_type = '<%= @cert_type %>'
 
-$VerbosePreference = 'Continue';
-$ErrorActionPreference = 'Stop';
+$VerbosePreference = 'Continue'
+$ErrorActionPreference = 'Stop'
 
-# check if $cert_path is valid
+## check given paths
+if (-not (Test-Path -Path $key_path)) {
+    Write-Verbose "Certificate $key_path does not exist";
+    exit 1;
+} 
 if (-not (Test-Path -Path $cert_path)) {
     Write-Verbose "Certificate $cert_path does not exist";
     exit 1;
+} 
+
+## PEM KEY -> KEY
+if (-not (Test-Path -Path "$key_path.key")) {
+    openssl rsa -in $key_path -out "$key_path.key";
+    if (-not $?) {
+        Write-Verbose "Private key extracted from pem file and stored in $key_path.key"
+    } else {
+        Write-Verbose "Failed to extract private key from pem file";
+        exit 1;
+    }
+} else {
+    Write-Verbose "Private key $key_path.key already exists"
 }
 
-# function to convert certificate to byte array for later import
-Function GetBytesFromPEM( [string] $pemString, [string] $section = 'CERTIFICATE' )
-{
-    $header = "-----BEGIN {0}-----" -f $section;
-    $footer = "-----END {0}-----" -f $section;
-    $start = $pemString.IndexOf($header, [System.StringComparison]::Ordinal);
-
-    if( $start -lt 0 ) {return $null};
-
-    $start += $header.Length;
-
-    $end = $pemString.IndexOf($footer, $start, [System.StringComparison]::Ordinal) - $start;
-    if( $end -lt 0 ) {return $null};
-
-    return [System.Convert]::FromBase64String( $pemString.Substring( $start, $end ) );
+## PEM CERT + KEY -> PFX
+if (-not (Test-Path -Path "$cert_path.pfx")) {
+    openssl pkcs12 -export -out "$cert_path.pfx" -inkey "$key_path.key" -in $cert_path -passout pass:;
+    if (-not $?) {
+        Write-Verbose "Pfx file created with private key and stored in $cert_path.pfx"
+    } else {
+        Write-Verbose "Failed to create pfx file";
+        exit 1;
+    }
+} else {
+    Write-Verbose "Pfx file $cert_path.pfx already exists"
 }
 
-# read cert
-$pem = [System.IO.File]::ReadAllText( $cert_path );
-# convert cert to byte array
-$certBuffer = GetBytesFromPEM( $pem );
-
-# create new cert from byte array
 $pfx = new-object System.Security.Cryptography.X509Certificates.X509Certificate2;
-$pfx.import( [System.Byte[]] $certBuffer );
+$pfx.import( "$cert_path.pfx" );
 $cert_thumbprint = $pfx.Thumbprint.ToUpper();
 Write-Verbose "Certificate thumbprint is $cert_thumbprint";
 
-# check current cert store
 Write-Verbose "Opening certificate store $cert_type ...";
 $storename = $null;
 switch ($cert_type)
